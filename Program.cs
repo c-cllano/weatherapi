f72dd3a0a662e7c6c0671af3d4997c5990d1c5c8
Carlos Eduardo Llano Escalon <c-cllano@olimpiait.com>
2026-03-12 11:40:40 -0500

    Modificacion de yaml3

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
-// Habilita Swagger/OpenAPI SIEMPRE CELL cambio 9 SIEMPRE, para facilitar el desarrollo y pruebas
+// Habilita Swagger/OpenAPI SIEMPRE CELL cambio 10 SIEMPRE, para facilitar el desarrollo y pruebas
 app.MapOpenApi();
 app.UseSwagger();
 app.UseSwaggerUI();
