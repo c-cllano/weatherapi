f2eab4a919a963cef7140f377cd9685337a8213c
Carlos Eduardo Llano Escalon <c-cllano@olimpiait.com>
2026-03-12 16:05:02 -0500

    Ahora SI

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
-// Habilita Swagger/OpenAPI SIEMPRE CELL cambio 14 SIEMPRE, para facilitar el desarrollo y pruebas
+// Habilita Swagger/OpenAPI SIEMPRE CELL cambio 15 SIEMPRE, para facilitar el desarrollo y pruebas
 app.MapOpenApi();
 app.UseSwagger();
 app.UseSwaggerUI();
