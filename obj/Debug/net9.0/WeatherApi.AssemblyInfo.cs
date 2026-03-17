c65675fbb3856247db25d953c697624c1c6c0160
Carlos Eduardo Llano Escalon <c-cllano@olimpiait.com>
2026-03-17 10:56:20 -0500

    Nuevos cambios

--- obj/Debug/net9.0/WeatherApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/WeatherApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41cc936356879dd22ff8fdb11a7549b084f142ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85d50395a69094e7d8231f0070c6d0e14a3cb374")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeatherApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
